7e843b1ea96b1a5ad81ca87d37b27f5a3450681d
Po <bborodach@gmail.com>
2022-12-12 10:49:40 -0500

    updated file struct

--- seeder/json_seeder/webhook.py
+++
@@ -1,8 +0,0 @@
-import requests 
-def send_json_to_https_endpoint(fake_logs, endpoint_url, num_times,headers):
-    for i in range(num_times):
-          response = requests.post(endpoint_url, json=fake_logs, headers=headers)
-          if response.status_code != 200:
-            raise ValueError(f'Failed to send json to endpoint. Status code: {response.status_code}')
-
-      
