1f31ca15ac938db6c310ed1d72564a840a7a33cc
Brandon Borodach <bborodach@gmail.com>
2022-12-13 16:18:14 -0500

    updated spelling error

--- seeder/json_seeder/json_seeder.py
+++ seeder/json_seeder/json_seeder.py
@@ -14,9 +14,9 @@ time.sleep(2)
 
 def main(): 
   def data_validation(filename):
-        ALLOWED_EXTENSTIONS = {".json"}
+        ALLOWED_EXTENSIONS = {".json"}
         extension = None
-        while extension not in ALLOWED_EXTENSTIONS:
+        while extension not in ALLOWED_EXTENSIONS:
             if filename == sys.argv[0]:
                 print("Please provide a valid json file with the script. Ex: 'python3 test.py data.json' ")
                 return False
